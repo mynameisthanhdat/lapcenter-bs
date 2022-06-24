852ed6f04ac07ffd59d216e27259b4992469cc1e
DatPham <mynameisthanhdat@gmail.com>
2022-06-24 21:34:05 +0700

    Design base product detail page

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Login from "./pages/login";
 import Register from './pages/register';
+import ProductDetail from './pages/productDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,6 +21,7 @@ root.render(
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
